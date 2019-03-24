3ffab8025ebc1a830aacbfb5800e080ee3459033
Андрей Пулин <kaoru-san@yandex.ru>
2019-03-24 19:13:17 +0300

    sendMail обновил форматирование подвал сделал фотки поменял

--- build/sendMail.php
+++ build/sendMail.php
@@ -1,19 +1,19 @@
 <?php
 if((isset($_POST['message'])&&$_POST['message']!="")&&(isset($_POST['contact'])&&$_POST['contact']!="")){
-    $to = 'kaoru-san@yandex.ru';
-    $subject = 'Обратный звонок';
-    $message = '
-                <html>
-                    <head>
-                        <title>'.$subject.'</title>
-                    </head>
-                    <body>
-                        <p>Почта: '.$_POST['contact'].'</p>
-                        <p>Сообщение: '.$_POST['message'].'</p>
-                    </body>
-                </html>';
-    $headers  = "Content-type: text/html; charset=utf-8 \r\n";
-    $headers .= "From: Отправитель <example@example.com>\r\n";
+    $to = 'kaoru-san@yandex.ru';
+    $subject = 'С сайта Pink Sweater';
+    $message = '
+            <html>
+                <head>
+                    <title>'.$subject.'</title>
+                </head>
+                    <body>
+                        <p>Контакт отправителя: '.$_POST['contact'].'</p>
+                        <p>Сообщение: '.$_POST['message'].'</p>
+                    </body>
+                </html>';
+    $headers = "Content-type: text/html; charset=utf-8 \r\n";
+    $headers .= "From: Robot <fedorova@ps-pr.ru>\r\n";
 
-    mail($to, $subject, $message, $headers);
+    mail($to, $subject, $message, $headers);
 }
